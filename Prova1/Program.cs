1b0a9573b19a03c4f4537976ee9fd06383157d31
girleide2 <147008816+girleide2@users.noreply.github.com>
2023-11-29 12:11:44 -0300

    Add files via upload
    Commit obrigatório 6

--- Prova1/Program.cs
+++ Prova1/Program.cs
@@ -50,29 +50,38 @@ public class Relatorios{
     Medico medico = new Medico();
     Paciente paciente = new Paciente();
 
-public void RelatorioIdadeMedico(){
-/*var DadosMedico = DadosMedico.Where(p => p.Idade >= minimo && p.Idade<= maximo);
+/*public void RelatorioIdadeMedico(){
 
-                Console.WriteLine($"Medicos com idade entre {minimo} e {maximo}:");
-                foreach (var med in DadosMedico)
-                {
-                    Console.WriteLine($"Nome: {med.Nome}, Data de nascimento: {med.DataNascimento}, CPF: {med.CPF}, CRM");
-                } */
-}
-public void RelatorioIdadePaciente(){
     Console.WriteLine("Digite a idade mínima:");
     int minimo = int.Parse(Console.ReadLine());
     Console.WriteLine("Digite a idade máxima:");
-    int maxima = int.Parse(Console.ReadLine());
-    
-    var DadosPaciente = DadosPaciente.Where(p => p.Idade >= minimo && p.Idade<= maximo);
+    int maximo = int.Parse(Console.ReadLine());
+    //fazer validação ppra idade
+
+    var MedicoPorIdade = medico.DadosMedico.Where(p => p.Idade >= minimo && p.Idade<= maximo);
 
     Console.WriteLine($"Medicos com idade entre {minimo} e {maximo}:");
-        foreach (var pac in DadosPaciente)
+        foreach (var med in paciente.DadosMedico)
+            {
+                Console.WriteLine($"Nome: {med.Nome}, Data de nascimento: {med.DataNascimento}, CPF: {med.CPF}, Sexo:{med.Sexo}, Sintomas:{med.Sintomas} ");
+            } 
+}
+
+   public void RelatorioIdadePaciente(){
+ Console.WriteLine("Digite a idade mínima:");
+    int minimo = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite a idade máxima:");
+    int maximo = int.Parse(Console.ReadLine());
+    //fazer validação ppra idade
+
+    var PacientePorIdade = paciente.DadosPaciente.Where(p => p.Idade >= minimo && p.Idade<= maximo);
+
+    Console.WriteLine($"Medicos com idade entre {minimo} e {maximo}:");
+        foreach (var pac in paciente.DadosPaciente)
             {
                 Console.WriteLine($"Nome: {pac.Nome}, Data de nascimento: {pac.DataNascimento}, CPF: {pac.CPF}, Sexo:{pac.Sexo}, Sintomas:{pac.Sintomas} ");
             } 
-}
+}*/
 public void PacienteSexo(){
     string sexo;
 
@@ -81,7 +90,7 @@ public void PacienteSexo(){
 
     var PacientePorSexo = paciente.DadosPaciente.Where(p => p.Sexo == sexo).ToList();
 
-    Console.WriteLine($"Paciente com o sexo {sexo}");
+    Console.WriteLine($"Pacientes com o sexo {sexo}");
     foreach (var Paciente in paciente.DadosPaciente)
     {
         Console.WriteLine($"Nome: {Paciente.Nome}, Data de nascimento: {Paciente.DataNascimento}, CPF: {Paciente.CPF}, Sexo:{Paciente.Sexo}, Sintomas:{Paciente.Sintomas} ");
@@ -112,7 +121,14 @@ public class Program{
     static void Main(){
         Medico medico = new Medico();
         Paciente paciente = new Paciente();
+        Relatorios relatorio = new Relatorios();
 
+
+    paciente.pegaDadosPaciente();
+    relatorio.PacienteSexo();
+    relatorio.PacienteSintoma();
+
+    
     }
 }
 
